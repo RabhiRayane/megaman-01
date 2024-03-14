d2f08534ebc3d9fef06efdb790fa40d4ef81a4ae
RabhiRayane <e2382642@cmaisonneuve.qc.ca>
2024-03-14 10:17:31 -0400

    Initial commit

---
+++ Assets/Scripts/ControlerPersonnage.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Gestion de déplacement et du saut du personnage à l'aide des touches : a, d et w      
+* Gestion des détections de collision entre le personnage et les objets du jeu  
+* Par: Vahik Toroussian
+* Modifié: 5/12/2018
+*/
+public class ControlerPersonnage : MonoBehaviour
+{    
+    float vitesseX;      //vitesse horizontale actuelle
+    public float vitesseXMax;   //vitesse horizontale Maximale désirée
+    float vitesseY;      //vitesse verticale 
+    public float vitesseSaut;   //vitesse de saut désirée
+
+    /* Détection des touches et modification de la vitesse de déplacement;
+       "a" et "d" pour avancer et reculer, "w" pour sauter
+    */
+    void Update ()
+    {
+        // déplacement vers la gauche
+        if (Input.GetKey("a"))
+        {
+            vitesseX = -vitesseXMax;
+            GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+        else if (Input.GetKey("d"))   //déplacement vers la droite
+        {
+            vitesseX = vitesseXMax;
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            vitesseX = GetComponent<Rigidbody2D>().velocity.x;  //mémorise vitesse actuelle en X
+        }
+
+        // sauter l'objet à l'aide la touche "w"
+        if (Input.GetKeyDown("w"))
+        {
+            vitesseY = vitesseSaut;
+            GetComponent<Animator>().SetBool("saut", true);
+        }
+        else
+        {
+            vitesseY = GetComponent<Rigidbody2D>().velocity.y;  //vitesse actuelle verticale
+        }
+        
+        //Applique les vitesses en X et Y
+        GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
+
+
+        //**************************Gestion des animaitons de course et de repos********************************
+        //Active l'animation de course si la vitesse de déplacement n'est pas 0, sinon le repos sera jouer par Animator
+        if (vitesseX > 0.01f || vitesseX < -0.1f)
+        {
+            GetComponent<Animator>().SetBool("marche", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("marche", false);
+        }
+      
+       
+       
+
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<Animator>().SetBool("saut", false);
+
+    }
+}
+
---
+++ Assets/Scripts/ExploserBombe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExploserBombe : MonoBehaviour {
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Détecte les colisions de l'objert Bombe
+    // Active l'animation de l'objet lorsque le terrain est touché
+    // Détruit l'Objet après un delais (à la fin de son animation) 
+    void OnCollisionEnter2D(Collision2D infoCollision)
+    {
+        // Si le terrain est touché alors active l'animation de l'objet et détruit le
+        if (infoCollision.gameObject.name == "gazon") 
+        {
+            GetComponent<Animator>().enabled = true;
+            Destroy(gameObject, 0.2f);
+        }
+    }
+}
